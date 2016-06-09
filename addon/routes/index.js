6b66bd8641d64527fd258a0b598197cf85de5045
Dan Gebhardt <dan@cerebris.com>
2016-06-09 16:57:40 -0400

    Add logging to show store injected into routes is a DS.Store

--- addon/routes/index.js
+++ addon/routes/index.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Route.extend({
   blogData: Ember.inject.service(),
 
   model() {
+    console.log('store service is an instanceof DS.Store', this.get('store') instanceof DS.Store);
+
     return this.get('blogData').posts;
   }
 });
